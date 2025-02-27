fd5da2d9964f7a2919f8b57aef79c59d7ba370c3
Vallérian Dicque <83017594+cidevalgames@users.noreply.github.com>
2025-02-27 11:38:48 +0100

    Intégration du projet initial de Térence

---
+++ Assets/Scripts/ImageTargetSize.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Vuforia;
+
+public class AdjustImageTargetSize : MonoBehaviour
+{
+    void Start()
+    {
+        ImageTargetBehaviour imageTarget = GetComponent<ImageTargetBehaviour>();
+
+        if (imageTarget != null)
+        {
+            Vector2 targetSize = imageTarget.GetSize();
+            Debug.Log("Taille correcte de l’image : " + targetSize.x + "m x " + targetSize.y + "m");
+
+            // Met à jour la scale
+            transform.localScale = new Vector3(targetSize.x, targetSize.y, 1);
+        }
+        else
+        {
+            Debug.LogError("L’Image Target n’a pas été trouvée !");
+        }
+    }
+
+    void Update()
+    {
+        // Vérification en continu pour voir si le scale est bien mis à jour
+        Debug.Log("Scale Actuel : " + transform.localScale);
+    }
+}
