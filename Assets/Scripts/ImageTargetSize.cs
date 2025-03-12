01ead9d1d47b1453e811b23520cced45340d028c
Vallérian Dicque <83017594+cidevalgames@users.noreply.github.com>
2025-03-12 21:57:16 +0100

    Installation du package de new Input System

--- Assets/Scripts/ImageTargetSize.cs
+++ Assets/Scripts/ImageTargetSize.cs
@@ -3,14 +3,14 @@ using Vuforia;
 
 public class AdjustImageTargetSize : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
         ImageTargetBehaviour imageTarget = GetComponent<ImageTargetBehaviour>();
 
         if (imageTarget != null)
         {
             Vector2 targetSize = imageTarget.GetSize();
-            Debug.Log("Taille correcte de l’image : " + targetSize.x + "m x " + targetSize.y + "m");
+            Debug.Log($"Taille correcte de l’image : {targetSize.x} m x {targetSize.y} m");
 
             // Met à jour la scale
             transform.localScale = new Vector3(targetSize.x, targetSize.y, 1);
@@ -24,6 +24,6 @@ public class AdjustImageTargetSize : MonoBehaviour
     void Update()
     {
         // Vérification en continu pour voir si le scale est bien mis à jour
-        Debug.Log("Scale Actuel : " + transform.localScale);
+        Debug.Log($"Scale Actuel : {transform.localScale}");
     }
 }
