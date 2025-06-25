ed995def906d32cfab2ec4de19d08a9f8160dca1
terencegb <91134582+terencegb@users.noreply.github.com>
2025-06-25 19:34:45 +0200

    ajout de la couverture et dernière de couv et prblm avec build

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVl0VHVHci8vLy8vQUFBQm1WSE5hZlZUdlVGUG1zMDNrWTkvSGNKbEROaWdwUndCRk9DMVB1cnNpVWdQWUsxSDhrdWFwSUw2ZkxteFFBeEYzeUFEeUxoRFFKeW1VVlNTSitGYk5FbldEMmlsd0dkZGZWUDZFNS90SmFWREljenZCMUs4QmZXdFppc0R5Misxb1JiQ0UxcXpBZUNkMTBJMzUrUWp3emdjb1YrL3c0ZVdhYS9oNS9aWlQwMjdSMFpMRER1QUFQU2Ywd3pvcFg4MDhyM0Y3UGUxZi9MenQ2L2JVajJQN2txUWs3cTMxQnovT1lRQTFxMWYxY2ltMFdKU3RmMEo5V01TcjNpWEN6TTIwWUQrK0hBSkxlRDVuZlVhZDhzYXdKVFNFdlRMMW96RndOZUhYUXVpNXhPSUpGd2ZmSkdHRWVwdEhMVFN6SXhqY3EzM0ptaHJWNWJPQmpkeVo4OHZMdjJoSUw3dFczMVU5WlVyemZjWC90cDA=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
